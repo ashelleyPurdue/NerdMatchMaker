ba3c123c90e942add78689d6caa15f3c4d36cda9
alex <meyer104@purdue.edu>
2017-02-08 17:19:54 -0500

    redid folder system and set up a basic node.js server

--- cs408/drop.sql
+++
@@ -1,9 +0,0 @@
-drop Table Matches;
-drop Table messages;
-drop Table UserLanguage;
-drop Table BlockedUsers;
-drop Table User_Interests;
-drop Table Interests;
-drop Table Language;
-drop table User;
-
