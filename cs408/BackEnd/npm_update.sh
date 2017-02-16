21e0540f13fb18863f593357559d493c017c9ee7
Alex Shelley <ashelley@purdue.edu>
2017-02-16 13:02:53 -0500

    Added a message to npm_update.

--- cs408/BackEnd/npm_update.sh
+++ cs408/BackEnd/npm_update.sh
@@ -1,5 +1,9 @@
+echo "Updating npm packages.  Please wait..."
+
 npm install -g typescript
 npm install @types/node --save-dev
 npm install express
 npm link body-parser
 npm install mysql
+
+echo "NPM packages updated."
