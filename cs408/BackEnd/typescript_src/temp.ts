b459c30fadf23c123559f4cf49671a863f530087
Alex Shelley <ashelley@purdue.edu>
2017-02-22 11:02:49 -0500

    All functions in temp.ts are now exported the typescript way.

--- cs408/BackEnd/typescript_src/temp.ts
+++ cs408/BackEnd/typescript_src/temp.ts
@@ -1,15 +1,15 @@
 //Force this file to be a module
-/*export interface UselessInterface {
-}*/
+export interface UselessInterface {
+}
 
 //TODO change functions to deal with res.
-var con;
+export var con;
 //IS the test called a sucess or not
-var success;
+export var success;
 //will set the error to this and it can read it on the test side
-var error;
+export var error;
 
-var createCon = function() {
+export function createCon() {
     //count = 0;
     var mysql = require("mysql");
     var config = require("../config");
@@ -23,11 +23,12 @@ var createCon = function() {
     })
     return con;
 };
+
 /*this function will create an account in sql and return 0 if the account was created successfully, else it will return -1 if username exist or -2 if sql error happens
 json will be {UserName: "Not Null",Password "Not Null(I will salt this)",Picture: "Null",Birthday : "00/00/0000",Gender: "M","F,"MF",GenderInto: "M","F","MF",Location: "Not Null",InARelationship:"false"}
 */
 //inside callback will deal with two functions on error and on success
-var createAccount = function(json, callback, res) {
+export function createAccount(json, callback, res) {
     //TODO salt passwords
     con.query('Insert Into User Set ?', json, function(err, rows) {
         if (err) {
@@ -36,15 +37,17 @@ var createAccount = function(json, callback, res) {
             callback.success(rows, json, res, callback);
         }
     });
-};
+}
+
 //this function will deal with call back for if not error
-var createAccountCallback = function(rows, json, res, uselessParameter) {
+export function createAccountCallback(rows, json, res, uselessParameter) {
     //have it login so that way it can get the userID and test to make sure everything works right
     //TODO fix login
     var callback = {success:loginTest,Empty:loginEmptySet,error:genSQLError};
     login(json, res,callback);
-};
-var createAccountError = function(err, json, res, callback) {
+}
+
+export function createAccountError(err, json, res, callback) {
     var userName = { UserName: json.UserName };
     con.query('Select * from User where ?', userName, function(err2, rows) {
         if (err2){
@@ -64,7 +67,7 @@ var createAccountError = function(err, json, res, callback) {
 give {UserName: name,oldPassword:"password",newPassword:"password"}
 */
 /*need to call login first to check that password is correct if not user can just change password*/
-var editPassword = function(json,res,callback){
+export function editPassword(json,res,callback){
   con.query('Update User Set password = ? where UserID = ?',[json.newPassword,json.UserID],function(err,row){
     if(err){
       callback.error(err,json,res,callback,con);  
@@ -74,19 +77,22 @@ var editPassword = function(json,res,callback){
     }
   });
   return 0;
-};
-var genSuccess = function(rows,json,res,callback){
+}
+
+export function genSuccess(rows,json,res,callback){
   console.log("genSuccess");
   res.send({success:0});
-};
-var loginForEdPassSuc = function(rows,json,res,callback){
+}
+
+export var loginForEdPassSuc = function(rows,json,res,callback){
   //console.log("printing login json"+json);
   var call = {error:genSQLError,success:genSuccess};
   editPassword(json,res,call);
-}
+};
+
 //Give it {UserName: Not Null,Password: Not Null}
 //returns ID or -1 if invalid password or username or -2 if sql error
-var login = function(json, res, callback) {
+export var login = function(json, res, callback) {
     var q = con.query('Select * from User where UserName = ? AND Password = ?', [json.UserName,json.Password], function(err, rows) {
         //console.log(q);
         if (err) {
@@ -99,7 +105,8 @@ var login = function(json, res, callback) {
     });
     return 0;
 };
-var loginWithID = function(json, res, callback) {
+
+export var loginWithID = function(json, res, callback) {
     var q = con.query('Select * from User where UserID = ? AND Password = ?', [json.UserID,json.oldPassword], function(err, rows) {
         //console.log(q);
         if (err) {
@@ -112,16 +119,18 @@ var loginWithID = function(json, res, callback) {
     });
     return 0;
 };
-var loginTest = function(rows, json, res, callback) {
+
+export var loginTest = function(rows, json, res, callback) {
     var lbj = {UserID: rows[0].UserID };
     console.log(rows);
     res.send(rows[0]);
 };
-var loginEmptySet = function(res,callback) {
+
+export var loginEmptySet = function(res,callback) {
     res.send({ Error: -1 });
 };
 
-var genSQLError = function(err, json, res, callback, con){
+export var genSQLError = function(err, json, res, callback, con){
   console.log("genSQLError");
   res.send({Error:-2});
 };
@@ -133,7 +142,7 @@ var genSQLError = function(err, json, res, callback, con){
 //if preference does not exist it will add it to the list
 //getPreferences will return list of preferences.
 //callback is an object that is used to communicate with the testing framework.
-var addUserPref = function(json, callback, res){
+export var addUserPref = function(json, callback, res){
 	getPrefID(json.Name, function(id:number){
 		
 		//If we didn't find that ID, create it and use that as ID.
@@ -154,7 +163,7 @@ var addUserPref = function(json, callback, res){
 	});
 };
 
-var addUserPref_weHaveID = function(id, json, res, callback){
+export var addUserPref_weHaveID = function(id, json, res, callback){
 	//TODO: Deal with adding ID
 	//Error if id is -1
 	if (json.UserID < 1){
@@ -185,7 +194,7 @@ var addUserPref_weHaveID = function(id, json, res, callback){
 //Add a preference with {Name:"Not Null",Description:"Null"}
 //returns id if no error or -1 if error
 //The "return value" is actualy going to be the first argument of calback.
-var addPref = function(name_and_desc, returnFunc) {
+export var addPref = function(name_and_desc, returnFunc) {
 	con.query('Insert into Interests Set ?', name_and_desc, function(err, rows){
 		
 		//Check for errors
@@ -203,7 +212,7 @@ var addPref = function(name_and_desc, returnFunc) {
 
 //takes Name of pref and returns Id of pref, if -1 does not exist, if -2 sql error
 //The "return value" is actually going to be the first argument of callback.
-var getPrefID = function(Name, returnFunc) {
+export var getPrefID = function(Name, returnFunc) {
 	//Query for the id
 	con.query('Select * from Interests where Name = ?', [Name], function(err, rows){
 		//Check for errors
@@ -227,7 +236,7 @@ var getPrefID = function(Name, returnFunc) {
 
 /* Beginning of getUserPref saga */
 //Give it {UserID:num} in json
-var getUserPref = function(json, callback, res){
+export var getUserPref = function(json, callback, res){
 	//Gets a list of all prefs for the given user
 	
 	//Check if UserID is invalid
@@ -270,7 +279,7 @@ var getUserPref = function(json, callback, res){
 
 //Will return a list of preferences {Name:"String",Description:"Null"}
 //if error returns [{Name:"Error"}]
-var getPrefs = function(json, callback, res) {
+export var getPrefs = function(json, callback, res) {
     con.query('Select * from Interests', null, function(err, rows) {
         if (err) {
             callback.error(err, json, res, callback, con);
@@ -279,12 +288,14 @@ var getPrefs = function(json, callback, res) {
         }
     });
 };
-var getPrefsSuccess = function(rows, json, res, callback) {
+
+export var getPrefsSuccess = function(rows, json, res, callback) {
   res.send(rows);
 };
+
 //give it json of {UserID_1:,UserID_2,Message}
 //returns {Error:}
-var insertMessage = function(json,callback,res){
+export var insertMessage = function(json,callback,res){
 	//add check method to check if they are a match or not
 	con.query("UPDATE Matches Set IsBlocked=true,BlockingID=? where (UserID1 = ? AND UserID2 = ?) OR (UserID2 = ? AND UserID1 = ?)",
 		[json.UserID1,json.UserID1,json.UserID2,json.UserID1,json.UserID2],function(err,rows){
@@ -307,8 +318,9 @@ var insertMessage = function(json,callback,res){
 	});
 
 };
+
 //TODO test this function over command line
-var getMessages = function(json,callback,res){
+export var getMessages = function(json,callback,res){
 	con.query(`Select * from Message where (UserID1 = ? AND UserID2 = ?) OR (UserID1 = ? AND UserID2 = ? sort by MessageID`,[json.UserID1,json.UserID2,json.UserID2,json.UserID1],function(err,rows){
     	if (err) {
             callback.error(err, json, res, callback, con);
@@ -317,9 +329,10 @@ var getMessages = function(json,callback,res){
         }
     });
 };
+
 //give it json of {UserID1:,UserID2}
 //returns {Error:}
-var blockUser = function(json,callback,res){
+export var blockUser = function(json,callback,res){
     con.query("UPDATE Matches Set IsBlocked=true,BlockingID=? where (UserID1 = ? AND UserID2 = ?) OR (UserID2 = ? AND UserID1 = ?)",
 		[json.UserID1,json.UserID1,json.UserID2,json.UserID1,json.UserID2],function(err,rows){
     	if (err) {
@@ -329,9 +342,10 @@ var blockUser = function(json,callback,res){
         }
     });
 };
+
 //give it json of {UserID}
 //returns list of users and userIDs of matches
-var getMatches = function(json,callback,res){
+export var getMatches = function(json,callback,res){
         con.query("Select * from Matches where (UserID1 = ? OR UserID2 = ?) AND IsBlocked = false", 
 				  	[json.UserID1], function (err, rows) {
         if (err) {
@@ -343,10 +357,11 @@ var getMatches = function(json,callback,res){
     });
 };
 
+/*
 exports.insertMessage = insertMessage;
 exports.getMessages = getMessages;
 exports.createAccount = createAccount;
-exports.createCon = createCon;
+//exports.createCon = createCon;
 exports.login = login;
 exports.editPassword = editPassword;
 exports.con = con;
@@ -364,3 +379,4 @@ exports.getUserPref = getUserPref;
 exports.getPrefsSuccess = getPrefsSuccess;
 exports.loginWithID = loginWithID;
 exports.genSuccess = genSuccess;
+*/
