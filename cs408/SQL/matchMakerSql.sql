922ce6cf3bb4da31ed6850f44241fe917fe29367
meyeralex31 <meyer104@purdue.edu>
2017-03-01 19:33:15 -0500

    change MAtches and get rid of Password in login

--- cs408/SQL/matchMakerSql.sql
+++ cs408/SQL/matchMakerSql.sql
@@ -1,5 +1,5 @@
 Select * from User as U1
-Join User as U2 on U1.UserID != U2.UserID
+Join User as U2 on (U1.UserID != U2.UserID AND U1.UserID < U2.UserID)
 where U1.GenderInto = U2.Gender AND U1.Gender = U2.GenderInto
 And (U1.age > U2.minAge AND U1.age < U2.maxAge) 
 And (U2.age > U1.minAge AND U2.age < U1.maxAge)
